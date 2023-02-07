70749dcf62259582de813a9fa15e97967054c1a7
Hank Weiss <10811528@my.uvu.edu>
2023-02-06 17:29:42 -0700

    removed items that called for "2d"

--- DetectTrigger.cs
+++ DetectTrigger.cs
@@ -4,8 +4,9 @@ using UnityEngine.Events;
 public class DetectTrigger : MonoBehaviour
 {
     public UnityEvent triggerEvent;
-    void OnTriggerEnter2D(Collider2D other)  // Once the Trigger has been entered record collision in the argument variable "other"
+    void OnTriggerEnter(Collider other)  // Once the Trigger has been entered record collision in the argument variable "other"
     {
         triggerEvent.Invoke();
+        Debug.Log("triggerdetected");
     }
 }
